c6b442ff2959da8e16396b8741a63a9979aa3cc7
Johnny Trinh <johnny.trinh@hotmail.se>
2024-02-01 16:13:49 +0100

    Import BrowserRouter and Nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Nav from "./nav/Nav.js"
 
 const App = () => {
   return <div className="App">App</div>
