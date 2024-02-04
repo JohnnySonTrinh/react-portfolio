a3850b0c640b9d8727a51d72fe17721d0dbb3b84
Johnny Trinh <johnny.trinh@hotmail.se>
2024-02-04 03:18:39 +0100

    Create frontend and backend skills data

---
+++ src/skills/skillsData.js
@@ -0,0 +1,72 @@
+const skills = {
+  1: [
+    {
+      title: "HTML",
+      level: 6,
+    },
+    {
+      title: "CSS",
+      level: 5,
+    },
+    {
+      title: "Bootstrap",
+      level: 6,
+    },
+    {
+      title: "TailwindCSS",
+      level: 3,
+    },
+    {
+      title: "JavaScript",
+      level: 5,
+    },
+    {
+      title: "React",
+      level: 4,
+    },
+    {
+      title: "TypeScript",
+      level: 4,
+    },
+    {
+      title: "Figma",
+      level: 1,
+    },
+  ],
+  2: [
+    {
+      title: "Python",
+      level: 6,
+    },
+    {
+      title: "Django",
+      level: 4,
+    },
+    {
+      title: "Node.js",
+      level: 5,
+    },
+    {
+      title: "Next.js",
+      level: 4,
+    },
+    {
+      title: "Git",
+      level: 5,
+    },
+    {
+      title: "SQL",
+      level: 3,
+    },
+    {
+      title: "MongoDB",
+      level: 4,
+    },
+    {
+      title: "Express.js",
+      level: 3,
+    },
+  ],
+}
+
+export default skills
