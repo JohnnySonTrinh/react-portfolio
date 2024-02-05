4b913c67b87f403142d265d58ba7afd8eea8ef2e
Johnny Trinh <johnny.trinh@hotmail.se>
2024-02-05 17:51:51 +0100

    Extends hackathon component

---
+++ src/hackathons/Hackathons.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+import HackathonsMenu from "./HackathonsMenu"
+
+export default class Hackathons extends Component {
+  render() {
+    return (
+      <>
+        <HackathonsMenu />
+      </>
+    )
+  }
+}
