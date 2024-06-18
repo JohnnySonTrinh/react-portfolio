21adc8cb8524837e574a3b9ac6f785f499237b3b
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-06-18 15:51:47 +0200

    refactor(Avatar): Component to use template literals for class names

--- src/avatar/Avatar.js
+++ src/avatar/Avatar.js
@@ -1,13 +1,13 @@
 // Importing required modules and assets
-import PropTypes from "prop-types" // Prop-types for typechecking of props
-import avatarImage from "../assets/avatar-image.webp" // Importing avatar image
-import "../styles/avatar.css" // Importing CSS for the avatar
+import PropTypes from "prop-types"; // Prop-types for typechecking of props
+import avatarImage from "../assets/avatar-image.webp"; // Importing avatar image
+import "../styles/avatar.css"; // Importing CSS for the avatar
 
 // Avatar component
 const Avatar = ({ page }) => {
   // Constructing class names based on the page prop
-  const avatarClass = `avatar ${page}`
-  const spanClass = `shadow-overlay-${page}`
+  const avatarClass = `avatar ${page}`;
+  const spanClass = `shadow-overlay-${page}`;
 
   // Returning the Avatar component
   return (
@@ -15,13 +15,13 @@ const Avatar = ({ page }) => {
       <span className={spanClass}></span>
       <img src={avatarImage} className={avatarClass} alt="avatar" />
     </>
-  )
-}
+  );
+};
 
 // Defining propTypes for the Avatar component
 Avatar.propTypes = {
   page: PropTypes.string.isRequired, // page prop is required and should be a string
-}
+};
 
 // Exporting the Avatar component
-export default Avatar
+export default Avatar;
