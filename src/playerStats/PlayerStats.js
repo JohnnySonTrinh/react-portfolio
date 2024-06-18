b642ca4b51b649cc0db3c12cdbcd9d3777fcc110
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-06-18 13:51:48 +0200

    chore: Update npm dependency

--- src/playerStats/PlayerStats.js
+++ src/playerStats/PlayerStats.js
@@ -1,12 +1,11 @@
 // Importing necessary modules and assets
-import React from "react" // React library for building user interfaces
-import "../styles/playerStats.css" // CSS styles for the PlayerStats component
-import Linkedin from "../assets/linkedin.png" // LinkedIn icon
-import Github from "../assets/github.png" // GitHub icon
+import React from "react"; // React library for building user interfaces
+import "../styles/playerStats.css"; // CSS styles for the PlayerStats component
+import Linkedin from "../assets/linkedin.png"; // LinkedIn icon
+import Github from "../assets/github.png"; // GitHub icon
 
 // PlayerStats component
 const PlayerStats = () => {
-  // PlayerStats component
   return (
     <div id="playerStats">
       <h1>JOHNNY TRINH LEVEL 27</h1>
@@ -17,18 +16,23 @@ const PlayerStats = () => {
       <h2>Fullstack Developer</h2>
       <div className="icon-container">
         <a
-          href="https://www.linkedin.com/in/johnny-trinh-732755123/"
+          href="https://www.linkedin.com/in/johnny-trinh-dev/"
           target="_blank"
+          rel="noopener noreferrer"
         >
           <img src={Linkedin} alt="Linkedin" className="icons" />
         </a>
-        <a href="https://github.com/JohnnySonTrinh/" target="_blank">
+        <a
+          href="https://github.com/JohnnySonTrinh/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <img src={Github} alt="Github" className="icons" />
         </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
 // Exporting the PlayerStats component
-export default PlayerStats
+export default PlayerStats;
