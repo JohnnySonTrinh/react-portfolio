1a9f99da474f6c1eac5c50dff7c66e1d92d44c43
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-06-22 20:47:35 +0200

    feat: Add HamburgerMenu and responssive design

---
+++ src/nav/HamburgerMenu.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const HamburgerMenu = ({ isOpen, toggleMenu }) => {
+  return (
+    <div className={`hamburger-menu ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+      <div className="bar"></div>
+      <div className="bar"></div>
+      <div className="bar"></div>
+    </div>
+  );
+};
+
+export default HamburgerMenu;
