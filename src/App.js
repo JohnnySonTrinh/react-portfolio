a69e1e323ed263e843979a90b27d5815305fa996
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-06-23 21:44:11 +0200

    Refactor App.js and PlayerStats.js components

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-// Importing necessary modules and components
-import React from "react" // React library for building user interfaces
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom" // React Router for routing
-import Nav from "./nav/Nav" // Navigation component
-import About from "./about/About" // About component
-import Skills from "./skills/Skills" // Skills component
-import Projects from "./projects/Projects" // Projects component
-import Hackathons from "./hackathons/Hackathons" // Hackathons component
-import Contact from "./contact/Contact" // Contact component
-import "./styles/app.css" // Global styles
-import Background from "./background/Background" // Background component
-import PlayerStats from "./playerStats/PlayerStats" // PlayerStats component
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./nav/Nav";
+import About from "./about/About";
+import Skills from "./skills/Skills";
+import Projects from "./projects/Projects";
+import Hackathons from "./hackathons/Hackathons";
+import Contact from "./contact/Contact";
+import "./styles/app.css";
+import Background from "./background/Background";
+import PlayerStats from "./components/playerStats/PlayerStats";
+import { CurrentUserProvider } from "./context/CurrentUserContext";
 
 // Main App component
 const App = () => {
   // Rendering the App component
   return (
-    <Router>
-      <Nav />
-      <Background />
-      <Routes>
-        <Route path="/" element={<About />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/hackathons" element={<Hackathons />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <PlayerStats />
-    </Router>
-  )
-}
+    <CurrentUserProvider>
+      <Router>
+        <Nav />
+        <Background />
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/hackathons" element={<Hackathons />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <PlayerStats />
+      </Router>
+    </CurrentUserProvider>
+  );
+};
 
 // Exporting the App component
-export default App
+export default App;
