f65a20be806c1e2979c6b8bcf762e1526ca12271
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-06-25 19:29:37 +0200

    Refactor(Nav): Component and related files to use components folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Nav from "./nav/Nav";
+import Nav from "./components/nav/Nav";
 import About from "./pages/about/About";
 import Skills from "./skills/Skills";
 import Projects from "./projects/Projects";
