61904056c5065cb51edf25014ffcaf330cbf0361
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-06-25 20:27:55 +0200

    refactor: Move Contact component and related files to pages folder

---
+++ src/pages/contact/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ContactMenu from "./ContactMenu";
+import "../../styles/contact.css";
+
+// Contact component
+const Contact = () => {
+  // Rendering the Contact component
+  return (
+    <>
+      <ContactMenu />
+    </>
+  );
+};
+
+// Exporting the Contact component
+export default Contact;
