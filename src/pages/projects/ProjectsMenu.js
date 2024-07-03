4fc698781ec96e6b24679938636ca40faecdf1ef
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-07-03 21:27:22 +0200

    Feat(projects): Add pagination functionality to ProjectsMenu

--- src/pages/projects/ProjectsMenu.js
+++ src/pages/projects/ProjectsMenu.js
@@ -6,10 +6,18 @@ import "../../styles/projectsMenu.css";
 // ProjectsMenu component
 const ProjectsMenu = () => {
   const [activeProject, setActiveProject] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const projectsPerPage = 3; // Number of projects per page
 
   // Function to handle the project click
   const handleProjectClick = (project) => {
     setActiveProject(project);
+  };
+
+  // Function to handle page change
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+    setActiveProject((page - 1) * projectsPerPage + 1);
   };
 
   // Function to render the content
@@ -35,26 +43,49 @@ const ProjectsMenu = () => {
     );
   };
 
-  const projectItems = ["PROJECT ONE", "PROJECT TWO", "PROJECT THREE"];
+  const projectItems = projects.slice(
+    (currentPage - 1) * projectsPerPage,
+    currentPage * projectsPerPage
+  );
   const activeProjectData = projects[activeProject - 1];
+  const totalPages = Math.ceil(projects.length / projectsPerPage);
 
   return (
     <div className="project-menu">
       <div className="project-items-container">
-        {projectItems.map((item, index) => (
+        {projectItems.map((project, index) => (
           <div
             key={index}
             className={classNames("project-item", {
-              activeProject: activeProject === index + 1,
+              activeProject:
+                activeProject ===
+                index + 1 + (currentPage - 1) * projectsPerPage,
             })}
-            onClick={() => handleProjectClick(index + 1)}
+            onClick={() =>
+              handleProjectClick(
+                index + 1 + (currentPage - 1) * projectsPerPage
+              )
+            }
           >
-            <h2 className="title">{item}</h2>
+            <h2 className="title">{project.title}</h2>
           </div>
         ))}
       </div>
       <div className="project-sub-container">
         {renderContent(activeProjectData)}
+        <div className="pagination">
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index}
+              className={classNames("pagination-button", {
+                activePage: currentPage === index + 1,
+              })}
+              onClick={() => handlePageChange(index + 1)}
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
