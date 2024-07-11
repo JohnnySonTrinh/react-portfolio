dd3a3f09e0b5103097812d5c7bbb90cb98db0f8f
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-07-11 11:51:37 +0200

    Chore(home page): Add comments and animations

--- src/components/playerStats/PlayerStats.jsx
+++ src/components/playerStats/PlayerStats.jsx
@@ -5,13 +5,17 @@ import Github from "../../assets/github.png";
 import { LINKEDIN_URL, GITHUB_URL } from "./playerData";
 import { usePlayerStats } from "../../context/PlayerStatsContext";
 
+// PlayerStats component
 const PlayerStats = () => {
+  // Extracting age and barWidth from the usePlayerStats hook
   const { age, barWidth } = usePlayerStats();
 
   return (
-    <section id="playerStats" role="contentinfo">
+    // Main section with fade-in animation and role for accessibility
+    <section className="fade-in" id="playerStats" role="contentinfo">
       <h1>Johnny Trinh Level {age}</h1>
       <div id="playerStats__lines">
+        {/* Dynamic width for the thick line based on barWidth */}
         <span id="playerStats__lines__thick" style={{ width: barWidth }}></span>
         <span id="playerStats__lines__thin"></span>
       </div>
@@ -38,5 +42,5 @@ const PlayerStats = () => {
   );
 };
 
-// Exporting the PlayerStats component
+
 export default PlayerStats;
