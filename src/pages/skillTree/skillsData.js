96948494c5a9edc13ac5c3c91a5dd57a0eb37d27
Johnny Trinh <johnny.trinh@hotmail.se>
2024-08-01 20:32:30 +0200

    Add data for deployment

---
+++ src/pages/skillTree/skillsData.js
@@ -0,0 +1,31 @@
+const skillsData = {
+  frontend: [
+    { title: "HTML", icon: "devicon-html5-plain" },
+    { title: "CSS", icon: "devicon-css3-plain" },
+    { title: "Bootstrap", icon: "devicon-bootstrap-plain" },
+    { title: "TailwindCSS", icon: "devicon-tailwindcss-plain" },
+    { title: "JavaScript", icon: "devicon-javascript-plain" },
+    { title: "React", icon: "devicon-react-original" },
+    { title: "TypeScript", icon: "devicon-typescript-plain" },
+    { title: "Figma", icon: "devicon-figma-plain" },
+  ],
+  backend: [
+    { title: "Python", icon: "devicon-python-plain" },
+    { title: "Django", icon: "devicon-django-plain" },
+    { title: "Node.js", icon: "devicon-nodejs-plain" },
+    { title: "Next.js", icon: "devicon-nextjs-plain" },
+    { title: "Git", icon: "devicon-git-plain" },
+    { title: "SQL", icon: "devicon-mysql-plain" },
+    { title: "MongoDB", icon: "devicon-mongodb-plain" },
+    { title: "Express.js", icon: "devicon-express-original" },
+  ],
+  fullstack: [
+    { title: "Git", icon: "devicon-git-plain" },
+    { title: "Github", icon: "devicon-github-original" },
+    { title: "Heroku", icon: "devicon-heroku-original" },
+    { title: "Vercel", icon: "devicon-vercel-plain" },
+    { title: "npm", icon: "devicon-npm-original-wordmark" },
+  ],
+};
+
+export default skillsData;
