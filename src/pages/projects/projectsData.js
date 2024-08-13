65f08e1da0d4de64e94ea1e95c48f2c2eb25adfc
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-08-13 12:02:25 +0200

    Update projects menu styles and pagination

--- src/pages/projects/projectsData.js
+++ src/pages/projects/projectsData.js
@@ -1,9 +1,6 @@
-// Importing necessary modules and assets
-import projectOne from "../../assets/project-1.webp"; // Project 1 image
-import projectTwo from "../../assets/project-2.webp"; // Project 2 image
-import projectThree from "../../assets/project-3.webp"; // Project 3 image
+import projectOne from "../../assets/project-1.webp";
+import projectTwo from "../../assets/project-2.webp";
 
-// function to get the projects data
 const projects = [
   {
     title: "Tic Tac Tactics",
@@ -16,11 +13,31 @@ const projects = [
           provide a fun, engaging, and interactive gaming experience for both
           Valorant enthusiasts and casual gamers alike.
         </p>
+        <p className="hover-description">Tech stack: HTML, CSS, JavaScript</p>
       </>
     ),
     github: "https://github.com/JohnnySonTrinh/valorant-tic-tac-tactics",
     live: "https://johnnysontrinh.github.io/valorant-tic-tac-tactics/",
   },
+  {
+    title: "Star Review",
+    image:
+      "https://github.com/JohnnySonTrinh/review-app/raw/main/documentation/update-mockup.png",
+    description: (
+      <>
+        <p>
+          Star Review is the ultimate platform for developers to collaborate,
+          review, and enhance code quality through seamless peer feedback.
+          Whether you're a beginner seeking guidance or an experienced coder
+          looking to refine your skills.
+        </p>
+        <p className="hover-description">Tech stack: HTML, CSS, JavaScript</p>
+      </>
+    ),
+    github: "https://github.com/JohnnySonTrinh/review-api",
+    live: "https://star-review-app-fb4aac8cda63.herokuapp.com",
+  },
+
   {
     title: "Coach Profile",
     image: projectTwo,
@@ -30,26 +47,63 @@ const projects = [
           This project is more than just a portfolio; it's a vibrant showcase of
           gaming passion, skills, and experiences.
         </p>
+        <p className="hover-description">Tech stack: HTML, CSS, JavaScript</p>
       </>
     ),
     github: "https://github.com/JohnnySonTrinh/a-gamers-portfolio-site",
     live: "https://johnnysontrinh.github.io/a-gamers-portfolio-site",
   },
   {
-    title: "Event App",
-    image: projectThree,
+    title: "Snake",
+    image:
+      "https://raw.githubusercontent.com/JohnnySonTrinh/snake-game/main/documentation/mockup.png",
     description: (
       <>
         <p>
-          Book and learn helpful tips from 3,168+ mentors in world-class
-          companies with our global community.
+          Snake game implemented in Python. which runs in mock terminal on
+          Heroku. The games features a fun and interactive rendition of the
+          classic snake game we all know and love.
         </p>
+        <p className="hover-description">Tech stack: HTML, CSS, JavaScript</p>
       </>
     ),
-    github: "https://github.com/JohnnySonTrinh/modern-car-showcase-app",
-    live: "https://event-app-pink-eight.vercel.app/",
+    github: "https://github.com/JohnnySonTrinh/snake-game",
+    live: "https://snake-eat-food-game-b47e36d72bf2.herokuapp.com",
+  },
+  {
+    title: "eBuddies",
+    image:
+      "https://github.com/JohnnySonTrinh/ebuddies/raw/main/documentation/mockup.png",
+    description: (
+      <>
+        <p>
+          Community platform designed to connect gamers with shared interests
+          and hobbies. Our mission is to create a welcoming space where users
+          can find and interact with like-minded peers, share experiences, and
+          build meaningful connections.
+        </p>
+        <p className="hover-description">Tech stack: HTML, CSS, JavaScript</p>
+      </>
+    ),
+    github: "https://github.com/JohnnySonTrinh/ebuddies",
+    live: "https://ebuddies-42967ce5447d.herokuapp.com",
+  },
+  {
+    title: "Portfolio",
+    image: "https://i.imgur.com/uSr4R1c.png",
+    description: (
+      <>
+        <p>
+          Personal portfolio site designed to showcase my projects, skills, and
+          experiences. This project serves as a digital resume and interactive
+          platform for potential employers and collaborators.
+        </p>
+        <p className="hover-description">Tech stack: HTML, CSS, JavaScript</p>
+      </>
+    ),
+    github: "https://github.com/JohnnySonTrinh/react-portfolio",
+    live: "https:/johnnytrinh.se",
   },
 ];
 
-// Exporting the projects data
 export default projects;
