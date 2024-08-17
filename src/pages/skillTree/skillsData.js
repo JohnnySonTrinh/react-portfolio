16fdf6a05886cdc88d02d7a096b6514c485a3fdf
JohnnySonTrinh <139331088+JohnnySonTrinh@users.noreply.github.com>
2024-08-17 23:53:19 +0200

    Add docker update skill data order

--- src/pages/skillTree/skillsData.js
+++ src/pages/skillTree/skillsData.js
@@ -8,13 +8,14 @@ const skillsData = {
     { title: "TailwindCSS", icon: "devicon-tailwindcss-plain" },
     { title: "TypeScript", icon: "devicon-typescript-plain" },
     { title: "Figma", icon: "devicon-figma-plain" },
+    { title: "Postman", icon: "devicon-postman-plain" },
   ],
   backend: [
     { title: "Python", icon: "devicon-python-plain" },
     { title: "Django", icon: "devicon-django-plain" },
+    { title: "DjangoREST", icon: "devicon-djangorest-plain" },
     { title: "Node.js", icon: "devicon-nodejs-plain" },
     { title: "Next.js", icon: "devicon-nextjs-plain" },
-    { title: "Git", icon: "devicon-git-plain" },
     { title: "PostgreSQL", icon: "devicon-postgresql-plain" },
     { title: "MongoDB", icon: "devicon-mongodb-plain" },
     { title: "Express.js", icon: "devicon-express-original" },
@@ -24,6 +25,7 @@ const skillsData = {
     { title: "Github", icon: "devicon-github-original" },
     { title: "Heroku", icon: "devicon-heroku-original" },
     { title: "Vercel", icon: "devicon-vercel-plain" },
+    { title: "Docker", icon: "devicon-docker-plain" },
     { title: "npm", icon: "devicon-npm-original-wordmark" },
   ],
 };
