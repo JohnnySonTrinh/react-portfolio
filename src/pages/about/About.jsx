1d011c65c70dcdf0a1e9ca74d6287fe1f821d5a5
JohnnySonTrinh <johnny.trinh@hotmail.se>
2024-11-22 14:14:46 +0100

    Refactor: Replace AboutContext with useAbout hook

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,13 +1,12 @@
 import Avatar from "../../components/avatar/Avatar";
 import AboutMenu from "./AboutMenu";
-import { AboutProvider } from "../../context/AboutContext";
 
 const About = () => {
   return (
-    <AboutProvider>
+    <>
       <Avatar page="about" />
       <AboutMenu />
-    </AboutProvider>
+    </>
   );
 };
 
