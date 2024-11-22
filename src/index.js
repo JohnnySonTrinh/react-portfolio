1bfb345a65e90335ec26442f7298521bc7e29143
JohnnySonTrinh <johnny.trinh@hotmail.se>
2024-11-22 18:54:24 +0100

    Fix import order in App and index files for consistency

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react" // React library for building user interfaces
-import ReactDOM from "react-dom/client" // React library for building user interfaces
-import App from "./App" // App component
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
 // Render the App component
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 // Compare this snippet from src/index.js:
-root.render(<App />)
+root.render(<App />);
