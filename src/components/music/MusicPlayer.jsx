e8d876e7ee3d8bff2e10589e558370eef07edc1b
JohnnySonTrinh <johnny.trinh@hotmail.se>
2024-11-25 22:08:04 +0100

    Add animation and music NotFound page

---
+++ src/components/music/MusicPlayer.jsx
@@ -0,0 +1,89 @@
+import { useEffect, useRef, useState } from "react";
+import Sound from "../../assets/the-uprising.mp3";
+
+const MusicPlayer = ({ audioData }) => {
+  const audioRef = useRef(null);
+  const [audioCtx, setAudioCtx] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    let analyser;
+    let dataArray;
+    let bufferLength;
+    const localAudioRef = audioRef.current; // Save the current ref value to a local variable
+
+    const initializeAudio = () => {
+      if (!audioCtx) {
+        const context = new (window.AudioContext || window.webkitAudioContext)();
+        analyser = context.createAnalyser();
+        analyser.fftSize = 256;
+
+        const source = context.createMediaElementSource(localAudioRef);
+        source.connect(analyser);
+        source.connect(context.destination);
+
+        bufferLength = analyser.frequencyBinCount;
+        dataArray = new Uint8Array(bufferLength);
+
+        setAudioCtx(context);
+      }
+    };
+
+    const handleAudioToggle = () => {
+      if (audioCtx.state === "suspended") {
+        audioCtx.resume();
+      }
+      if (localAudioRef.paused) {
+        localAudioRef.play();
+        setIsPlaying(true);
+      } else {
+        localAudioRef.pause();
+        setIsPlaying(false);
+      }
+    };
+
+    const updateAudioData = () => {
+      if (analyser) {
+        analyser.getByteFrequencyData(dataArray);
+        const average = dataArray.reduce((sum, value) => sum + value, 0) / bufferLength;
+        audioData.current = average / 256.0;
+      }
+      requestAnimationFrame(updateAudioData);
+    };
+
+    // Initialize audio context and event listener
+    document.addEventListener("click", initializeAudio);
+    document.addEventListener("click", handleAudioToggle);
+    updateAudioData();
+
+    return () => {
+      // Cleanup resources
+      document.removeEventListener("click", initializeAudio);
+      document.removeEventListener("click", handleAudioToggle);
+      if (localAudioRef) localAudioRef.pause();
+      if (audioCtx) audioCtx.close();
+    };
+  }, [audioCtx, audioData]);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          if (audioRef.current.paused) {
+            audioRef.current.play();
+            setIsPlaying(true);
+          } else {
+            audioRef.current.pause();
+            setIsPlaying(false);
+          }
+        }}
+        aria-label={isPlaying ? "Pause Music" : "Play Music"}
+      >
+        {isPlaying ? "Pause Music" : "Play Music"}
+      </button>
+      <audio ref={audioRef} src={Sound} crossOrigin="anonymous" volume="0.01" />
+    </div>
+  );
+};
+
+export default MusicPlayer;
--- src/pages/404page/NotFoundMenu.jsx
+++ src/pages/404page/NotFoundMenu.jsx
@@ -1,12 +1,22 @@
+import { useRef } from "react";
 import { Link } from "react-router-dom";
+import useWebGLAnimation from "../../hooks/useWebGLAninimation";
+import MusicPlayer from "../../components/music/MusicPlayer";
 import "../../styles/notFound.css";
 
 const NotFound = () => {
+  const canvasRef = useRef(null);
+  const audioData = useRef(0);
+
+  useWebGLAnimation(canvasRef, audioData);
+
   return (
     <div className="not-found">
       <h1>404</h1>
-      <h2>LOOKS LIKE YOU ARE </h2>
+      <h2>LOOKS LIKE YOU ARE</h2>
       <h2>LOST IN SPACE</h2>
+      <canvas ref={canvasRef}></canvas>
+      <MusicPlayer audioData={audioData} />
       <Link to="/" className="home-button">
         Go back home
       </Link>
