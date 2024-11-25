33e2392ea99b4a7a3f709292a97b0548031b0edf
JohnnySonTrinh <johnny.trinh@hotmail.se>
2024-11-25 18:01:52 +0100

    Update background video green transition

--- src/components/background/Background.jsx
+++ src/components/background/Background.jsx
@@ -1,5 +1,5 @@
 import "../../styles/background.css";
-import video from "../../assets/bg-universe.mp4";
+import video from "../../assets/background-transition.mp4";
 import fallbackImage from "../../assets/fallback-image.webp";
 
 // Background component
