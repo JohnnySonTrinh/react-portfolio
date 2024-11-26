f199b853fa82da002017948fb4ae0446016adb07
JohnnySonTrinh <johnny.trinh@hotmail.se>
2024-11-26 16:09:17 +0100

    Add/Remove comments

--- src/utils/handleWheelScroll.js
+++ src/utils/handleWheelScroll.js
@@ -1,6 +1,9 @@
+// Desc: Handle wheel scroll event
 const handleWheelScroll = (e, options) => {
+  // Destructure options object
   const { currentIndex, setIndex, maxIndex } = options;
 
+  // Check if the wheel event is up or down
   if (e.deltaY > 0 && currentIndex < maxIndex) {
     setIndex(currentIndex + 1);
   } else if (e.deltaY < 0 && currentIndex > 0) {
