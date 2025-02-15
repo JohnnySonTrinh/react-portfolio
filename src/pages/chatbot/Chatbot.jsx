eb2037ca71f4dee7402e356c88018a9e52fcb330
JohnnySonTrinh <johnny.trinh@hotmail.se>
2025-02-15 16:12:55 +0100

    Add Chatbot page title and integrate ChatbotMenu component

--- src/pages/chatbot/Chatbot.jsx
+++ src/pages/chatbot/Chatbot.jsx
@@ -1,6 +1,15 @@
+import ChatbotMenu from "./ChatbotMenu"
+import useMetaTitle from "../../hooks/useMetaTitle";
+import pageTitles from "../../data/pageTitles";
+
+
 const Chatbot = () => {
+  // Set the meta title for the page
+  useMetaTitle(pageTitles.chatbot);
   return (
-    <div>Chatbot</div>
+    <>
+    <ChatbotMenu />
+  </>
   )
 }
 
