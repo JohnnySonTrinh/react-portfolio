c72e5727b5f9621d79a8c8ccb9cba32b9e2ba441
JohnnySonTrinh <johnny.trinh@hotmail.se>
2025-02-16 22:30:08 +0100

    Refactor Chatbot and ChatbotMenu

--- src/pages/chatbot/Chatbot.jsx
+++ src/pages/chatbot/Chatbot.jsx
@@ -1,16 +1,17 @@
-import ChatbotMenu from "./ChatbotMenu"
+import ChatbotMenu from "./ChatbotMenu";
 import useMetaTitle from "../../hooks/useMetaTitle";
 import pageTitles from "../../data/pageTitles";
-
 
 const Chatbot = () => {
   // Set the meta title for the page
   useMetaTitle(pageTitles.chatbot);
-  return (
-    <>
-    <ChatbotMenu />
-  </>
-  )
-}
 
-export default Chatbot
+  return (
+    // Chat interface will go here
+    <>
+      <ChatbotMenu />
+    </>
+  );
+};
+
+export default Chatbot;
--- src/pages/chatbot/ChatbotMenu.jsx
+++ src/pages/chatbot/ChatbotMenu.jsx
@@ -1,7 +1,10 @@
 const ChatbotMenu = () => {
   return (
-    <div>ChatbotMenu</div>
-  )
-}
+    <div>
+      <h2>AI Chatbot</h2>
+      <p>Work in progress...</p>
+    </div>
+  );
+};
 
-export default ChatbotMenu
+export default ChatbotMenu;
