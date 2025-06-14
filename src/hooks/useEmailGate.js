3ce1e10df7f8c67ad184c49466477157b43e0d2c
JohnnySonTrinh <johnny.trinh@hotmail.se>
2025-06-14 09:20:38 +0200

    Add email gate functionality

---
+++ src/hooks/useEmailGate.js
@@ -0,0 +1,28 @@
+// src/hooks/useEmailGate.js
+import { useState } from "react";
+
+const useEmailGate = () => {
+  const [emailSubmitted, setEmailSubmitted] = useState(false);
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+
+  const handleEmailSubmit = () => {
+    const isValid = /^[^@]+@[^@]+\.[^@]+$/.test(email);
+    if (isValid) {
+      setError("");
+      setEmailSubmitted(true);
+    } else {
+      setError("Please enter a valid email.");
+    }
+  };
+
+  return {
+    emailSubmitted,
+    email,
+    setEmail,
+    error,
+    handleEmailSubmit,
+  };
+};
+
+export default useEmailGate;
